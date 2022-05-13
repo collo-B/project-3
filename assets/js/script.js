7775689118e5b05c287df2c95b3d022fd07d8f0e
collo-bii <ckipkoech17@gmail.com>
2022-05-13 19:17:28 +0300

    combine html pages to 1 page

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+ 
+ var c=0;
+
+ function quesa () {
+     var q1=console.quiz1;
+     if (q1=="Java");
+     c++;
+     console.log(c);
+ }
+
+ function quesb () {
+    var q2=console.quiz2;
+    if (q2=="Nairobi");
+    c++;
+    console.log(c);
+}
+function quesc () {
+    var q3=console.quiz3;
+    var a=5-3;
+    if (q3=="a");
+    c=c+2;
+    console.log(c);
+}
+function quesd () {
+    var q4=console.quiz4;
+    var b=9-5;
+    if (q4=="b");
+    c=c+2;
+    console.log(c);
+}
