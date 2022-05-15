975c3902cfe736d9390934f7db912baf509d9544
collo-bii <ckipkoech17@gmail.com>
2022-05-15 17:34:46 +0300

    organise body html add js for selecting answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,96 @@
- 
- var c=0;
-
- function quesa () {
-     var q1=console.quiz1;
-     if (q1=="Java");
-     c++;
-     console.log(c);
- }
-
- function quesb () {
-    var q2=console.quiz2;
-    if (q2=="Nairobi");
-    c++;
-    console.log(c);
-}
-function quesc () {
-    var q3=console.quiz3;
-    var a=5-3;
-    if (q3=="a");
-    c=c+2;
-    console.log(c);
-}
-function quesd () {
-    var q4=console.quiz4;
-    var b=9-5;
-    if (q4=="b");
-    c=c+2;
-    console.log(c);
-}
-function quese () {
-    var q5=console.quiz5;
-    var d=11+4;
-    if (q5=="d");
-    c=c+2;
-    console.log(c);
+function checkers() {
+  var correct = ["Java", "Nairobi", "2", "4", "15", "30", "66", "30", "5", "5"];
+  var score = 0;
+  var question1 = document.querySelector(
+    'input[name="question1"]:checked'
+  ).value;
+  var question2 = document.querySelector(
+    'input[name="question2"]:checked'
+  ).value;
+  var question3 = document.querySelector(
+    'input[name="question3"]:checked'
+  ).value;
+  var question4 = document.querySelector(
+    'input[name="question4"]:checked'
+  ).value;
+  var question5 = document.querySelector(
+    'input[name="question5"]:checked'
+  ).value;
+  var question6 = document.querySelector(
+    'input[name="question6"]:checked'
+  ).value;
+  var question7 = document.querySelector(
+    'input[name="question7"]:checked'
+  ).value;
+  var question8 = document.querySelector(
+    'input[name="question8"]:checked'
+  ).value;
+  var question9 = document.querySelector(
+    'input[name="question9"]:checked'
+  ).value;
+  var question10 = document.querySelector(
+    'input[name="question10"]:checked'
+  ).value;
+  var done = document.getElementById("done");
+  var quiz = document.getElementById("quiz");
+  if (question1 == correct[0]) {
+    score += 1;
+  } else if (question1 != correct[0]) {
+    score += 0;
+  }
+  if (question2 == correct[1]) {
+    score += 1;
+  } else if (question2 != correct[1]) {
+    score += 0;
+  }
+  if (question3 == correct[2]) {
+    score += 2;
+  } else if (question3 != correct[2]) {
+    score += 0;
+  }
+  if (question4 == correct[3]) {
+    score += 2;
+  } else if (question4 != correct[3]) {
+    score += 0;
+  }
+  if (question5 == correct[4]) {
+    score += 2;
+  } else if (question5 != correct[4]) {
+    score += 0;
+  }
+  if (question6 == correct[5]) {
+    score += 2;
+  } else if (question6 != correct[5]) {
+    score += 0;
+  }
+  if (question7 == correct[6]) {
+    score += 2;
+  } else if (question7 != correct[6]) {
+    score += 0;
+  }
+  if (question8 == correct[7]) {
+    score += 2;
+  } else if (question8 != correct[7]) {
+    score += 0;
+  }
+  if (question9 == correct[8]) {
+    score += 3;
+  } else if (question9 != correct[8]) {
+    score += 0;
+  }
+  if (question10 == correct[9]) {
+    score += 3;
+  } else if (question10 != correct[9]) {
+    score += 0;
+  }
+  quiz.style.display = "none";
+  if (score <= 5) {
+    done.textContent = "you scored ${score}. POOR!";
+  } else if (score <= 10) {
+    done.textContent = "you scored ${score}. FAIR";
+  } else if (score <= 15) {
+    done.textContent = "you scored ${score}. GOOD!";
+  } else if (score <= 20) {
+    done.textContent = "you scored ${score}. EXCELLENT!!";
+  }
 }
